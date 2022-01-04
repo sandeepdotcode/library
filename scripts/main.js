de9a0f46396d402f36c35abb21c1441a2f830e6c
Sandeep K K <sandeepdotcode@gmail.com>
2022-01-04 19:59:58 +0530

    Add function to display Cards

--- scripts/main.js
+++ scripts/main.js
@@ -15,9 +15,34 @@ function addBookToLibrary(title, author, pages, read) {
 addBookToLibrary("Subtle Art of Not Giving a Fuck", "Mark Manson", 224, "yes");
 addBookToLibrary("Grokking Algorithms", "Aditya", 256, "no");
 addBookToLibrary("Beautiful World, Where Are You", "Sally Rooney", 250, "no");
+displayBook();
 
 function displayBook() {
+    const libLen = myLibrary.length;
+    const libContainer = document.querySelector('.library-display');
 
+    for (let i = 0; i < libLen; i++) {
+        const card = document.createElement('div');
+        const titleText = document.createElement('h3');
+        const authorText = document.createElement('span');
+        const pagesText = document.createElement('span');
+        const readBtn = document.createElement('button');
+        const delBtn = document.createElement('button');
+
+        titleText.textContent = myLibrary[i].title;
+        authorText.textContent = myLibrary[i].author;
+        pagesText.textContent = myLibrary[i].pages;
+        readBtn.textContent = (myLibrary[i].read == 'yes') ? 'Read' : 'Not Read';
+        delBtn.textContent = 'Remove';
+
+        card.appendChild(titleText);
+        card.appendChild(authorText);
+        card.appendChild(pagesText);
+        card.appendChild(readBtn);
+        card.appendChild(delBtn);
+
+        libContainer.appendChild(card);
+    }
 }
 
 function showForm() {
